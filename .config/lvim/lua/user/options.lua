c6c7936f26edd11271587317bb2a363003b92c5e
Luis Augusto Nunes Marins <155187227+Luis-Marins@users.noreply.github.com>
2024-12-17 16:51:00 -0300

    Update options.lua
    Added comments to the code

--- .config/lvim/lua/user/options.lua
+++ .config/lvim/lua/user/options.lua
@@ -1,5 +1,5 @@
-vim.opt.relativenumber = true
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.relativenumber = true -- Enables relative line numbers in the editor.
+vim.opt.clipboard = 'unnamedplus' -- Configures Neovim to use the system clipboard.
 
-lvim.use_icons = true
-lvim.format_on_save = true
+lvim.use_icons = true -- Enables the use of icons in LunarVim.
+lvim.format_on_save = true -- Enables automatic code formatting every time you save a file.
