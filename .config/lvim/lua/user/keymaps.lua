3cd210643a4456d13d3d9385a61df97e3d4c1bd4
Luis Augusto Nunes Marins <155187227+Luis-Marins@users.noreply.github.com>
2025-01-09 11:46:24 -0300

    Update keymaps.lua
    removing the keymap for vertical split to avoid conflit with visual block

--- .config/lvim/lua/user/keymaps.lua
+++ .config/lvim/lua/user/keymaps.lua
@@ -2,7 +2,6 @@ lvim.keys.normal_mode["<C-s>"] = ":w<CR>" -- Keymap to save file
 lvim.keys.normal_mode["<C-q>"] = ":q<CR>" -- Keymap to quit current window (When one window quit Vim)
 lvim.keys.normal_mode["<C-d>"] = "<C-d>zz" -- Scroll window downwards in buffer and redraw cursor line at center of window
 lvim.keys.normal_mode["<C-u>"] = "<C-u>zz" -- Scroll window upwards in buffer and redraw cursor line at center of window
-lvim.keys.normal_mode["<C-v>"] = ":vsplit<CR>" -- Split current window vertically
 lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>" -- Close buffer
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>" -- Move to next buffer
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>" -- Move to previous buffer
