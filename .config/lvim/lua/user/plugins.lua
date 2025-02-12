70a05678e0d03001ec537ee0b0c35775a461e960
Luis Augusto Nunes Marins <155187227+Luis-Marins@users.noreply.github.com>
2025-02-12 19:57:08 -0300

    Update plugins.lua
    Adding hex.nvim plugin to see binaries of files

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -5,7 +5,7 @@ lvim.plugins = {
     cmd = { 'LiveServerStart', 'LiveServerStop' },
     config = function()
       require('live-server').setup({
-          args = { "--port=8080", "--browser=firefox-developer-edition" }
+        args = { "--port=8080", "--browser=firefox-developer-edition" }
       })
     end
   },
@@ -26,5 +26,29 @@ lvim.plugins = {
   },
   {
     'mg979/vim-visual-multi',
+  },
+  {
+    'RaafatTurki/hex.nvim',
+    -- defaults
+    require 'hex'.setup {
+
+      -- cli command used to dump hex data
+      dump_cmd = 'xxd -u',
+
+      -- cli command used to assemble from hex data
+      assemble_cmd = 'xxd -r',
+
+      -- function that runs on BufReadPre to determine if it's binary or not
+      is_file_binary_pre_read = function()
+        -- logic that determines if a buffer contains binary data or not
+        -- must return a bool
+      end,
+
+      -- function that runs on BufReadPost to determine if it's binary or not
+      is_file_binary_post_read = function()
+        -- logic that determines if a buffer contains binary data or not
+        -- must return a bool
+      end,
+    }
   },
 }
