9c59a5917efe74ddb1fe96ac0bb75db8e5a3dd22
Luis Augusto Nunes Marins <marinsaugusto18@gmail.com>
2025-08-09 02:07:40 -0300

    add AstroNvim configs

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,23 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.recipes.ai" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.utility.live-server-nvim" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  -- example of importing an entire language pack
+  -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.python" },
+  -- import/override with your plugins folder
+}
