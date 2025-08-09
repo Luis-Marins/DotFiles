24e94af4c63236ed5c982b665d32f8d3c7d3af20
Luis Augusto Nunes Marins <155187227+Luis-Marins@users.noreply.github.com>
2025-08-09 00:45:28 -0300

    Delete .config/lvim directory
    Migrate LunarVim to AstroNvim

--- .config/lvim/lua/user/keymaps.lua
+++
@@ -1,7 +0,0 @@
-lvim.keys.normal_mode["<C-s>"] = ":w<CR>"                   -- Keymap to save file
-lvim.keys.normal_mode["<C-q>"] = ":q<CR>"                   -- Keymap to quit current window (When one window quit Vim)
-lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"                  -- Scroll window downwards in buffer and redraw cursor line at center of window
-lvim.keys.normal_mode["<C-u>"] = "<C-u>zz"                  -- Scroll window upwards in buffer and redraw cursor line at center of window
-lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"          -- Close buffer
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>" -- Move to next buffer
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>" -- Move to previous buffer
--- .config/lvim/lua/user/options.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.relativenumber = true     -- Enables relative line numbers in the editor.
-vim.opt.clipboard = 'unnamedplus' -- Configures Neovim to use the system clipboard.
-
-lvim.use_icons = true             -- Enables the use of icons in LunarVim.
-lvim.format_on_save = true        -- Enables automatic code formatting every time you save a file.
